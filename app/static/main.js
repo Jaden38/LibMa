66b0773a22b19f2d56a5c77d18a653b77629f5cf
lkrys <lkrys@myges.com>
2024-11-26 09:26:27 +0100

    configuration front end

---
+++ app/static/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "./components/ui/button";
+
+ReactDOM.render(
+    <Button>Click Me</Button>,
+    document.getElementById("app")
+);
