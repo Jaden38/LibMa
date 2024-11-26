2df2098355817a0abc6f4b126e793a712ddd7c7d
Jaden38 <damien.nithard@gmail.com>
2024-11-26 11:29:37 +0100

    feat(Project): Setup tailwind, backend/frontend

--- app/static/main.js
+++
@@ -1,8 +0,0 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const { Button } = require("./components/ui/button");
-
-ReactDOM.render(
-    React.createElement(Button, null, "Click Me"),
-    document.getElementById("app")
-);
--- backend/app/views.py
+++ backend/app/views.py
@@ -17,3 +17,11 @@ def test_db():
         return jsonify([{'id': book.id, 'title': book.title} for book in books])
     except Exception as e:
         return f'Database error: {str(e)}'
+    
+    
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
