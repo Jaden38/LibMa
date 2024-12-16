f2f92bb2d36c9ac1c485efed702b1fd59ef43678
Jaden38 <damien.nithard@gmail.com>
2024-12-16 12:43:47 +0100

    feat(Backend): Add model script

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,3 @@
-from app import cli
-from app import views
-from app import models
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
@@ -13,11 +10,16 @@ pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{
-    os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT', 3306)}/{os.getenv('MYSQL_DATABASE')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT', 3306)}/{os.getenv('MYSQL_DATABASE')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+from app import models
+
+from app import views
+
+from app import cli
 
 if __name__ == '__main__':
     app.run()
