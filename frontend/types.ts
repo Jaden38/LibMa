eef609c2fa87c140eab5ed2299225a6f67e44306
Enzo Hugonnier <enzohugonn@gmail.com>
2024-12-17 11:57:21 +0100

    style(types): Refactor code

--- frontend/types.ts
+++ frontend/types.ts
@@ -1,31 +1,30 @@
 export interface IBorrow {
+  id: number;
+  begin_date: string;
+  end_date: string | null;
+  status: "en cours" | "terminé" | "en retard" | "annulé";
+  user: {
     id: number;
-    begin_date: string; // Date as a string (e.g., '2024-12-01')
-    end_date: string | null; // Nullable because it may be incomplete
-    status: "en cours" | "terminé" | "en retard" | "annulé";
-    user: {
-      id: number;
-      lastname: string;
-      firstname: string;
-    };
-    return_date: string | null; // Date as a string or null if not returned
-  }
-  
-  export interface IBook {
-    id: number;
-    title: string;
-    author: string;
-    genre: string;
-    category: string;
-    release_date: string; // Date as a string (e.g., '2024-12-01')
-    description: string;
-    image_url: string;
-  }
-  
-  export interface ISample {
-    id: number;
-    unique_code: string;
-    status: "disponible" | "emprunté" | "réservé" | "indisponible";
-    localization: string | null;
-  }
-  
+    lastname: string;
+    firstname: string;
+  };
+  return_date: string | null;
+}
+
+export interface IBook {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  category: string;
+  release_date: string;
+  description: string;
+  image_url: string;
+}
+
+export interface ISample {
+  id: number;
+  unique_code: string;
+  status: "disponible" | "emprunté" | "réservé" | "indisponible";
+  localization: string | null;
+}
