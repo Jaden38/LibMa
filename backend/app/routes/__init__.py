0bcaab1b307f0ee39bd44db0fbb668cf83dd0ad8
lkrys <lkrys@myges.com>
2024-12-17 15:13:51 +0100

    fix(librarians): Add blueprint

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,6 @@
 from .auth import auth_bp
 from .books import books_bp
+from .librarians import librarians_bp
 from .notifications import notifications_bp
 from .samples import samples_bp
 
@@ -7,5 +8,6 @@ __all__ = [
     'auth_bp',
     'books_bp',
     'samples_bp',
+    'librarians_bp',
     'notifications_bp'
 ]
