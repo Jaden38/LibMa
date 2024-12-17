1d229d08f527726c2b9d60fd9c232d81549e69f2
Jaden38 <damien.nithard@gmail.com>
2024-12-17 14:49:18 +0100

    refactor(Backend): Refactor controllers

--- backend/app/routes/samples.py
+++ backend/app/routes/samples.py
@@ -9,23 +9,23 @@ samples_bp = Blueprint('samples', __name__)
 @samples_bp.route("/<int:id>/emprunts")
 def get_borrowed_sample(id):
     try:
-        borrows = Borrow.query.filter_by(sample_id=id).order_by(Sample.begin_date.desc()).all()
+        borrows = Borrow.query.filter_by(sample_id=id).order_by(Borrow.begin_date.desc()).all()
         result = [
             {
                 "id": borrow.borrow_id,
                 "begin_date": borrow.begin_date.isoformat(),
                 "end_date": borrow.end_date.isoformat(),
-                "return_date": borrow.return_date.isoformat() if borrow.return_date else None,
-                "status": borrow.status,
+                "returned_at": borrow.returned_at.isoformat() if borrow.returned_at else None,
+                "borrow_status": borrow.borrow_status,
                 "user": {
-                    "id": borrow.user.id_user,
-                    "lastname": borrow.user.nom,
-                    "firstname": borrow.user.prenom,
+                    "id": borrow.user.user_id,
+                    "lastname": borrow.user.lastname,
+                    "firstname": borrow.user.firstname,
                 },
             }
             for borrow in borrows
         ]
         return jsonify(result)
     except Exception as e:
-        logger.error(f"Erreur lors de la récupération des emprunts de l'sample {id}: {str(e)}")
+        logger.error(f"Erreur lors de la récupération des emprunts de l'exemplaire {id}: {str(e)}")
         return jsonify({"error": "Erreur de DB:", "message": str(e)}), 500
